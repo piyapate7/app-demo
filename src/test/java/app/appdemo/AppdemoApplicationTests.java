440e4cba805df32b35ef2bc88e0bb11953e17d1b
Piyaphat Khajornpan <piyapat.khahornpan.stream.co.th>
2022-02-01 12:28:09 +0700

    init

---
+++ src/test/java/app/appdemo/AppdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package app.appdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
